6abd965eb7399feda2e5cadb3e9f01dec16400a6
rendnil <andrew.lindner.15@gmail.com>
2018-09-10 21:50:40 -0400

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::Base
   helper_method :game
+  helper_method :deck
+  helper_method :player_hand
+  helper_method :dealer_hand
 
   def game
     session[:game] ||= [ ]
+
   end
+
+  def deck
+    session[:deck] ||= [ ]
+  end
+
+  def player_hand
+    session[:player_hand] ||= [ ]
+  end
+
+  def dealer_hand
+    session[:dealer_hand] ||= [ ]
+  end
+
 
   # def add_player_hand_to_cookie(@blackjack.player_hand.push(@blackjack.deal(2)))
   #   game << @blackjack.player_hand.push(@blackjack.deal(2)).flatten!
--- app/controllers/blackjacks_controller.rb
+++ app/controllers/blackjacks_controller.rb
@@ -10,6 +10,11 @@ class BlackjacksController < ApplicationController
       # @blackjack.dealer_hand.push(@blackjack.deal(2)).flatten!
       # @blackjack.update(deck: @blackjack.deck, player_hand: @blackjack.player_hand, dealer_hand: @blackjack.dealer_hand)
       #
+        # session.delete :game
+        # session.delete :deck
+        # session.delete :player_hand
+        # session.delete :dealer_hand
+        session.clear
         @new_game =  Blackjack.create
         @new_game.shuffle
         @new_game.player_hand.push(@new_game.deal(2)).flatten!
@@ -17,11 +22,14 @@ class BlackjacksController < ApplicationController
         @new_game.update(deck: @new_game.deck, player_hand: @new_game.player_hand, dealer_hand: @new_game.dealer_hand)
 
         game << @new_game.id
+        (deck.push(@new_game.deck)).flatten!
+        (player_hand.push(@new_game.player_hand)).flatten!
+        (dealer_hand.push(@new_game.dealer_hand)).flatten!
     render :game
   end
 
   def cookies
-    # byebug
+    #byebug
     @blackjack = Blackjack.find(game.first)
   end
   # def hit
